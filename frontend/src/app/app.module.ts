25c208c0be8f348bccb0b3e409e10b2a9eb461c0
Daniel-BS-Dev <danielbenedito355@gmail.com>
2022-03-09 20:01:46 -0300

    header done

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HeaderComponent } from './component/template/header/header.component';
+
+// para usar na minha header
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule  // qualquer module que usa outro import
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // apponta para o meu app.component
 })
 export class AppModule { }
